80456bc5588da82ae6c1023f144afa7337527dc0
sharan <karthikeyansharan00@gmail.com>
2021-09-03 20:28:16 +0530

    server check code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
+import axios from 'axios';
 
 import HomePage from './pages/home/home.component';
 import CatogoriesPage from './pages/catogories/catogories.component';
@@ -10,8 +11,24 @@ import './App.css';
 
 import Header from './components/header/header.component';
 import Footer from './components/footer/footer.component';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [welcome, setWelcome] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('/api/users')
+      .then(function (response) {
+        console.log('SUCC');
+        console.log(response.data.h1);
+        setWelcome(response.data.h1);
+      })
+      .catch(function (error) {
+        console.log('ERROR');
+        console.log(error);
+      }, []);
+  });
   return (
     <div>
       <Header />
